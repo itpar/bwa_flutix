ba45421a6245b6b0e0169d600b9bce7e2fd16fd3
IT DevOps PAR <76098586+itpar@users.noreply.github.com>
2021-01-06 12:28:44 +0700

    Initial commit

---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+String apiKey = "aa73f8a89d37105b51144e43cba5657a";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
+
+PageEvent prevPageEvent;
+File imageFileToUpload;
